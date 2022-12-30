e82df07b3b7df803507d9a98b072ecd03cd11e02
Athul Nambiar <108534940+athul-22@users.noreply.github.com>
2022-12-30 23:24:23 +0530

    V2

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 function fun(){
-
-   
-    
-
     // CALLING ELEMENTS
     var loader = document.getElementById("loader");
     var textValue = document.querySelector("#text-box").value;
@@ -21,13 +17,14 @@ function fun(){
     
     setTimeout(() => {
         loader.style.display = "none";
-    }, 2000);
+        loader.style.alignContent = "center";
+    }, 3000);
 
     
     // WIKIPEDIA SEARCH 
     // FETCHING FROM WIKIPEDIA API
 
-    let wikiURL = 'https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=1&srsearch='+NewSTR;
+    let wikiURL = 'https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=10&srsearch='+NewSTR;
     console.log(wikiURL);
 
     fetch(wikiURL)
@@ -56,6 +53,7 @@ function finalresult(myArray){
      resultElement.id = resultElement;
      
         myArray.forEach(function(item){
+
             let itemTitle = item.title;
             let itemSnippet = item.snippet;
             let itemUrl = encodeURI(`https://en.wikipedia.org/wiki/${item.title}`);
@@ -68,24 +66,38 @@ function finalresult(myArray){
             // STYLING THE ELEMENTS
 
            resultElement.style.backgroundColor = "rgb(139, 253, 139)";
-           resultElement.style.color = " rgb(60, 60, 60)";
+           resultElement.style.color = "rgb(0, 0, 92)";
            resultElement.style.fontSize = "20px";
            resultElement.style.lineHeight = "30px";
-           resultElement.style.paddingLeft = "4a0px";
+           resultElement.style.paddingLeft = "20px";
+           resultElement.style.marginTop = "30px";
+           resultElement.style.paddingRight = "20px";
+           resultElement.style.paddingTop = "20px";
+           resultElement.style.paddingBottom = "20px";
+           resultElement.style.fontFamily = "san-serif";
+           resultElement.style.borderRadius = "10px";
+           resultElement.style.marginLeft = "20px";
+           resultElement.style.marginRight = "20px";
 
             // REPLACING HTML TAG
             var newSnippet = itemSnippet.replace( /(<([^>]+)>)/ig, '');
             resultElement.innerHTML = itemSnippet;
             // FINAL RESULT PRINTNG
-            document.getElementById("resultDes").style.visibility = "visible";
-            document.getElementById("resultDes").innerHTML = newSnippet;
-            console.log(newSnippet);
-         //   resultElement.innerText = newSnippet;
+            if(document.getElementById("resultDes").innerHTML != " "){
+                document.getElementById("resultDes").innerHTML = " ";
+            }else{
+                document.getElementById("resultDes").style.visibility = "visible";
+                document.getElementById("resultDes").innerHTML = newSnippet;
+                console.log(newSnippet);
+            }
             
+            //   resultElement.innerText = newSnippet;  
         })
-
-       
-        
+}
 
 
+function loadFun(){
+    setTimeout(() => {
+        document.getElementById("h4").style.display = "none";
+    }, 5000);
 }
